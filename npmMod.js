c0a458ba7af65be5bb2be1d82847a0afd6e45f29
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-12-30 17:36:00 +0530

    commit

--- npmMod.js
+++ npmMod.js
@@ -1,4 +1,4 @@
 import chalk from 'chalk'
 import validator from 'validator'
-const ans = validator.isEmail("abc@gail.com")
+const ans = validator.isEmail("abc@gmail.com")
 console.log(ans?(chalk.green.inverse(ans)):(chalk.red.inverse(ans)))
