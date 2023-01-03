bc58fa4643e63db7426c90b5f56638ea289aea46
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-03 12:06:39 +0530

    commit

---
+++ mongooseLearn.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://127.0.0.1:27017/learning').then(()=>{console.log("Connection successful")}).catch((err)=>{console.log(err)});
+const playlistSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    ctype:{
+        type:String,
+        required:true
+    },
+    videos:{
+        type:Number,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    active:{
+        type:Boolean
+    },
+    submittedDate:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+const Playlist = mongoose.model('Playlist', playlistSchema);
+
+const videos1 = new Playlist({ name: 'ReactJS', ctype:'Frontend', videos:61, author:"Def", active:false });
+videos1.save().then(() => console.log('Done'));
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
-stringify
+const Cat = mongoose.model('Cat', { name: String });
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
