3831d2d6fd2cd5639ad090b7d009e8fe3d00af57
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-03 12:24:22 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Node-Shorts
-# Node-Shorts
--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const Cat = mongoose.model('Cat', { name: String });
-
-// const kitty = new Cat({ name: 'Zildjian' });
-// kitty.save().then(() => console.log('meow'));
